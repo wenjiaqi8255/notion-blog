dfc1f5b6fb7fe349b263bf2cbd1cceb771f2771f
wenjiaqi8255 <127540860+wenjiaqi8255@users.noreply.github.com>
2023-07-08 16:45:57 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -49,16 +49,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+   navigationLinks: [
+     {
+       title: 'About',
+       pageId: 'c6b69e3542384b3a81747ac87e8b9422'
+     },
+     {
+       title: 'Contact',
+       pageId: '86aec2d8116247cc8d8847a16ef3da96'
+     }
+   ]
 })
